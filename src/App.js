cbea7259b372091ad57579c51855de0a7bce9386
tonykrapatony <tonykrapatony@gmail.com>
2023-06-06 15:10:07 +0300

    updated route

--- src/App.js
+++ src/App.js
@@ -7,36 +7,36 @@ import Error from './pages/Error';
 import "./app.scss";
 import Header from './components/header/Header';
 import Product from './pages/Product';
-import { useEffect } from 'react';
-import { addProductsToCartAction } from './store/cartReducer';
-import { useDispatch, useSelector } from 'react-redux';
-import saveStateToLocalStorage from './store/saveStateToLocalStorage';
+// import { useEffect } from 'react';
+// import { addProductsToCartAction } from './store/cartReducer';
+// import { useDispatch, useSelector } from 'react-redux';
+// import saveStateToLocalStorage from './store/saveStateToLocalStorage';
 
 function App() {
-  const dispatch = useDispatch()
-  const products = useSelector(state => state.products.products);
+  // const dispatch = useDispatch()
+  // const products = useSelector(state => state.products.products);
 
-  useEffect(() => {
-    window.addEventListener("beforeunload", handleBeforeUnload);
+  // useEffect(() => {
+  //   window.addEventListener("beforeunload", handleBeforeUnload);
 
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  });
+  //   return () => {
+  //     window.removeEventListener("beforeunload", handleBeforeUnload);
+  //   };
+  // });
 
-  useEffect(() => {
-    let obj = JSON.parse(localStorage.getItem('reduxState'));
-    console.log(obj)
-    if (Object.keys(obj).length !== 0) {
-      for (let key in obj) {
-        dispatch(addProductsToCartAction(obj[key]));
-      }
-    }
-  }, [dispatch])
+  // useEffect(() => {
+  //   let obj = JSON.parse(localStorage.getItem('reduxState'));
+  //   console.log(obj)
+  //   if (Object.keys(obj).length !== 0) {
+  //     for (let key in obj) {
+  //       dispatch(addProductsToCartAction(obj[key]));
+  //     }
+  //   }
+  // }, [dispatch])
 
-  const handleBeforeUnload = () => {
-    saveStateToLocalStorage(products);
-  };
+  // const handleBeforeUnload = () => {
+  //   saveStateToLocalStorage(products);
+  // };
 
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HashRouter>
+    <BrowserRouter>
         <Provider store={store}>
             <App />
         </Provider>
-    </HashRouter>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
